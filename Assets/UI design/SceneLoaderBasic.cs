0e8e27d3b02a166629a51aae2a51719ee60158e9
Riboz <74473384+Riboz@users.noreply.github.com>
2023-02-02 00:16:58 +0300

    a
    a

--- Assets/UI design/SceneLoaderBasic.cs
+++ Assets/UI design/SceneLoaderBasic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using GoogleMobileAds.Api;
+
 public class SceneLoaderBasic : MonoBehaviour
 {
     public Scene sceneToLoad;
@@ -92,41 +92,8 @@ public class SceneLoaderBasic : MonoBehaviour
     
     public void loadNextLevel()
     {
-        StartCoroutine(Show());
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    
         
     }
-    private InterstitialAd interstitial;
-
-private void RequestInterstitial()
-{
-    #if UNITY_ANDROID
-        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
-    #elif UNITY_IPHONE
-        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
-    #else
-        string adUnitId = "unexpected_platform";
-    #endif
-
-    // Initialize an InterstitialAd.
-    this.interstitial = new InterstitialAd(adUnitId);
-    // Create an empty ad request.
-    AdRequest request = new AdRequest.Builder().Build();
-    // Load the interstitial with the request.
-    this.interstitial.LoadAd(request);
-}
-
-
-public IEnumerator Show()
-{
-  if (this.interstitial.IsLoaded()) {
-    this.interstitial.Show();
-  }
-  yield return new WaitForSeconds(15f);
-       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-       yield break;
-  
-}  
-
-
 }
